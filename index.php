e6fd28f1c91b4cdab4d7cd9ba15ddaabff9b9497
Peace Iyiewuare <Peace@wireless-10-147-220-180.public.utexas.edu>
2018-01-11 13:01:30 -0600

    adding contact feature

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Masterpeace Records';
+    $to = 'masterpeace.tm@gmail.com';
+    $subject = 'Beat Inquiry';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $from)) {
+	    echo '<p>Your message has been sent!</p>';
+      	} else {
+      	    echo '<p>Something went wrong, go back and try again!</p>';
+      	}
+
+    }
+?>
